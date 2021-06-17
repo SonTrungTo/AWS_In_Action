79edc99ac29b134eb407c0a0193ca0f051a18c8c
SonTrungTo <son.trung.to@gmail.com>
2021-06-18 02:25:45 +0300

    aws bs finished!

--- chapter5/awsbs-environment.sh
+++ chapter5/awsbs-environment.sh
@@ -7,6 +7,12 @@ aws elasticbeanstalk create-application-version --application-name etherpad \
     --source-bundle "S3Bucket=awsinaction-code2,S3Key=chapter05/etherpad.zip"
 ## Creating application configuration(Node.js in AMI)
 SolutionStackName=$(aws elasticbeanstalk list-available-solution-stacks --output text \
-    --query "SolutionStacks[?contains(@,'running Node.js')] | [0]")
+    --query "SolutionStacks[?contains(@,'running Node.js')]|[0]")
 echo $SolutionStackName
 ## Launching an environment
+aws elasticbeanstalk create-environment --environment-name etherpad \
+    --application-name etherpad \
+    --option-settings Namespace=aws:elasticbeanstalk:environment,\
+OptionName=EnvironmentType,Value=SingleInstance \
+    --solution-stack-name "$SolutionStackName"  \
+    --version-label 1
