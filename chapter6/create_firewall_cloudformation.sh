f9f463e377c345a065142a1ae61356e85c098f93
SonTrungTo <son.trung.to@gmail.com>
2021-06-27 01:03:08 +0300

    firewall2.yaml added!

--- chapter6/create_firewall_cloudformation.sh
+++ chapter6/create_firewall_cloudformation.sh
@@ -6,7 +6,7 @@ SubnetId="$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VpcId" \
     --query "Subnets[0].SubnetId" --output text)"
 
 aws cloudformation create-stack --stack-name firewall --template-url \
-    https://s3.amazonaws.com/awsinaction-code2/chapter06/firewall1.yaml \
+    https://s3.amazonaws.com/awsinaction-code2/chapter06/firewall2.yaml \
     --parameters "ParameterKey=KeyName,ParameterValue=mykey" \
     "ParameterKey=VPC,ParameterValue=$VpcId" \
     "ParameterKey=Subnet,ParameterValue=$SubnetId"
