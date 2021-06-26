ceba3738ef2e141735de5972f9bf5cc3bbdb0f47
SonTrungTo <son.trung.to@gmail.com>
2021-06-27 00:28:06 +0300

    create firewall script added!

--- chapter6/create_firewall_cloudformation.sh
+++ chapter6/create_firewall_cloudformation.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-https://drive.google.com/uc?export=download&id=1KS-EariTaSgnq3CawQOyn_5WeApzUxgK
-
 VpcId="$(aws ec2 describe-vpcs --query "Vpcs[0].VpcId" --output text)"
 
+SubnetId="$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VpcId" \
+    --query "Subnets[0].SubnetId" --output text)"
+
+aws cloudformation create-stack --stack-name firewall --template-url \
+    https://s3.amazonaws.com/awsinaction-code2/chapter06/firewall1.yaml \
+    --parameters "ParameterKey=KeyName,ParameterValue=mykey" \
+    "ParameterKey=VPC,ParameterValue=$VpcId" \
+    "ParameterKey=Subnet,ParameterValue=$SubnetId"
+
+aws cloudformation wait stack-create-complete --stack-name firewall
+
+aws cloudformation describe-stacks --stack-name firewall \
+    --query "Stacks[0].Outputs"
