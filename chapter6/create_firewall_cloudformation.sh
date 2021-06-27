5f1a44afa0669d947bd7f5e2ce4f2fa2a28ce28e
SonTrungTo <son.trung.to@gmail.com>
2021-06-28 01:00:32 +0300

    bastion_host created! ch6 nearly done!

--- chapter6/create_firewall_cloudformation.sh
+++ chapter6/create_firewall_cloudformation.sh
@@ -5,11 +5,14 @@ VpcId="$(aws ec2 describe-vpcs --query "Vpcs[0].VpcId" --output text)"
 SubnetId="$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VpcId" \
     --query "Subnets[0].SubnetId" --output text)"
 
+PublicIp="$(curl -s https://api.ipify.org)"
+
 aws cloudformation create-stack --stack-name firewall --template-url \
-    https://s3.amazonaws.com/awsinaction-code2/chapter06/firewall2.yaml \
+    https://s3.amazonaws.com/awsinaction-code2/chapter06/firewall5.yaml \
     --parameters "ParameterKey=KeyName,ParameterValue=mykey" \
     "ParameterKey=VPC,ParameterValue=$VpcId" \
-    "ParameterKey=Subnet,ParameterValue=$SubnetId"
+    "ParameterKey=Subnet,ParameterValue=$SubnetId" \
+    "ParameterKey=IpForSSH,ParameterValue=$PublicIp"
 
 aws cloudformation wait stack-create-complete --stack-name firewall
 
